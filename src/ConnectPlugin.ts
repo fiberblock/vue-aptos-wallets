2001403ec823d04615bf976130c213aa054d1147
Binh Nguyen <nguyenthaibinh0131@gmail.com>
2022-11-03 16:21:13 +0700

    feat: package as typescript build

--- src/ConnectPlugin.ts
+++
@@ -1,3 +0,0 @@
-import { useWallets } from "./hooks/wallet.hook";
-
-export { useWallets };
--- src/types/shims/window.d.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-
-declare global {
-  interface Window {
-    ethereum: any;
-  }
-}
-
-export {};
